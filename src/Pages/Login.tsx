114abf0cd1749ea42956c357df9b558ac7f49b28
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2020-11-25 18:04:07 +0000

    Connect frontend with Firebase Auth
    This connects the frontend (sign in and asign up) to Firebase auth using the email and password method.
    The app now keeps track of user context, i.e if the user is signed in or not. If the user is signed in they are redirected
    to the home page. If not, they will be at the login page.

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import '../App.css';
 import {COLORS} from '../colors';
-
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
-import {Link} from 'react-router-dom';
 import Button from 'react-bootstrap/Button'
-import { Form } from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
+import {FirebaseContext, IFirebaseContext} from "../FirebaseContext";
+import {useHistory} from "react-router-dom";
 
 export default function LogIn() {
+    let history = useHistory();
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const firebaseContext: IFirebaseContext = useContext(FirebaseContext);
+    if (firebaseContext.firebase.auth().currentUser) {
+        history.push("/");
+    }
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        try {
+            await firebaseContext.firebase.auth().signInWithEmailAndPassword(email, password);
+            history.push("/");
+        } catch (error) {
+            alert(error);
+        }
+    }
+
     return (
 
-       
-        <div 
+        <div
             className="Landing"
             style={{
                 backgroundColor: COLORS.accent
             }}>
-  
-            
+
 
             <Container>
 
@@ -30,45 +46,54 @@ export default function LogIn() {
                         Welcome Back
 
                     </h1>
-                    <br> 
-                    </br>   
+                    <br>
+                    </br>
                 </Row>
-                
-                <Form>
+
+                <Form onSubmit={handleSubmit}>
                     <Form.Row>
-                        <Form.Group controlId="loginEmail">
+                        <Form.Group controlId="signInEmail">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control
+                                autoFocus
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={event => setEmail(event.target.value)}
+                            />
                             <Form.Text className="text-muted">
                             </Form.Text>
                         </Form.Group>
                     </Form.Row>
-                    
+
                     <Form.Row>
-                        <Form.Group controlId="loginPassword">
+                        <Form.Group controlId="signInPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={event => setPassword(event.target.value)}
+                            />
                         </Form.Group>
                     </Form.Row>
-                        
- 
-                        <Button className="Button"
+
+                    <Button className="Button"
+                            type="submit"
                             style={{
                                 color: COLORS.darkText,
-                                backgroundColor: COLORS.highlight,
-                                borderColor: COLORS.highlight,
-
+                                backgroundColor: COLORS.secondaryAccent,
+                                borderColor: COLORS.secondaryAccent,
                                 display: "inline",
                                 position: "absolute",
                                 top: 400,
-                                left: 200,
+                                left: 300,
                             }}
-                            as={Link} to="/login"
-                            >
-                                Log in
-                        </Button>
-                    </Form>
+                    >
+                        Log in
+                    </Button>
+                </Form>
             </Container>
-        
+
         </div>);
 }
--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import '../App.css';
 import {COLORS} from '../colors';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
-import {Link} from 'react-router-dom';
 import Button from 'react-bootstrap/Button'
-import { Form } from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
+import {FirebaseContext, IFirebaseContext} from "../FirebaseContext";
+import {useHistory} from "react-router-dom";
+
 
 export default function SignUp() {
+    let history = useHistory();
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const firebaseContext: IFirebaseContext = useContext(FirebaseContext);
 
+    if (firebaseContext.firebase.auth().currentUser) {
+        history.push("/");
+    }
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        try {
+            await firebaseContext.firebase.auth().createUserWithEmailAndPassword(email, password);
+            history.push("/");
+        } catch (error) {
+            alert(error);
+        }
+    }
 
     return (
-        <div 
+        <div
             className="Landing"
             style={{
                 backgroundColor: COLORS.accent
@@ -27,41 +46,50 @@ export default function SignUp() {
                         Welcome
                     </h1>
                 </Row>
-                
-                <Form>
+
+                <Form onSubmit={handleSubmit}>
                     <Form.Row>
                         <Form.Group controlId="signupEmail">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control
+                                autoFocus
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={event => setEmail(event.target.value)}
+                            />
                             <Form.Text className="text-muted">
                             </Form.Text>
                         </Form.Group>
                     </Form.Row>
-                    
+
                     <Form.Row>
                         <Form.Group controlId="signupPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={event => setPassword(event.target.value)}
+                            />
                         </Form.Group>
                     </Form.Row>
-                        
+
                     <Button className="Button"
-                        style={{
-                            color: COLORS.darkText,
-                            backgroundColor: COLORS.secondaryAccent,
-                            borderColor: COLORS.secondaryAccent,
-                        display: "inline",
-                        position: "absolute",
-                        top: 400,
-                        left: 300,
-                    }}
-                    as={Link} to="/signup"
+                            type="submit"
+                            style={{
+                                color: COLORS.darkText,
+                                backgroundColor: COLORS.secondaryAccent,
+                                borderColor: COLORS.secondaryAccent,
+                                display: "inline",
+                                position: "absolute",
+                                top: 400,
+                                left: 300,
+                            }}
                     >
                         Sign up
-                </Button>
-                    </Form>
-            
+                    </Button>
+                </Form>
             </Container>
-        
         </div>);
 }
